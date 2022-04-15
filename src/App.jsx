b3b3b8a105d55f1172b728153a30617882305db1
Ksanikavada <ilya.tix@yandex.ru>
2022-04-15 06:21:30 +0300

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -104,7 +104,7 @@ export const App = () => {
               <NotFound
                />
             }/>
-       </Routes>
+          </Routes>
         </Content>
         <Footer>
           <FooterContent />
